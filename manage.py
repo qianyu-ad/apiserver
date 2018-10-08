04e06fdb275c716472f865a95edfacab0bd7ab7c
mink <cangmean@outlook.com>
2018-10-08 16:06:10 +0800

    添加脚本目录

--- manage.py
+++ manage.py
@@ -5,7 +5,6 @@ import os
 from flask_script import Server, Manager
 from apiserver.app import create_app
 from apiserver.models import db, User, Article, Category, Site, Seo
-from apiserver.utils import random_date
 from flask_migrate import Migrate, MigrateCommand
 
 # pylint: disable=all
@@ -32,14 +31,6 @@ def drop():
 def runserver():
     os.system('gunicorn -c unicorn.py manage:app')
 
-
-@manager.command
-def random_date():
-    articles = Article.query.all()
-    for article in articles:
-        article.display_time = random_date()
-    
-    db.session.commit()
 
 manager.add_command('run', Server(
     host='0.0.0.0',
---
+++ scripts/random_date.py
@@ -0,0 +1,15 @@
+from apiserver.utils import random_date
+from apiserver.models import db, Article
+from manage import app
+
+
+def main():
+    with app.app_context():
+        articles = Article.query.all()
+        for article in articles:
+            article.display_time = random_date()
+        
+        db.session.commit()
+
+if __name__ == '__main__':
+    main()
